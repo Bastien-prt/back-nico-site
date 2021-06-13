6434ca5b92c0f79e000453216484cde990d868c5
Bastien Perrot <bastien.perrot@ekino.com>
2021-06-13 14:53:13 +0200

    hash password

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const ardoises = require("./ardoises/ardoises")
+const register = require("./users/registration")
+const login = require("./users/login")
+
 
 router.use("/ardoises", ardoises);
+router.use("/register", register);
+router.use("/login", login);
 
 module.exports = router;
--- routes/users/registration.js
+++ routes/users/registration.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const connection = require("../../config");
 
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 // GET ALL USERS
 router.get("/", (req, res) => {
     connection.query("SELECT * FROM users", (err, results) => {
@@ -19,13 +22,21 @@ router.post("/", (req, res) => {
     const username = req.body.username
     const password = req.body.password
 
-    connection.query("INSERT INTO users(username, password) VALUES(?, ?)", [username, password ], (err, result) => {
-      if (err) {
-        res.status(500).send("Error adding new article");
-      } else {
-        res.status(200).send("Successfully adding");
-      }
-    });
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+        if (err) {
+            console.log(err)
+        }
+        connection.query("INSERT INTO users(username, password) VALUES(?, ?)", 
+        [username, hash ], 
+        (err, result) => {
+            if (err) {
+              res.status(500).send("Error adding new article");
+            } else {
+              res.status(200).send("Successfully adding");
+            }
+          });
+    })
+    
   });
 
 
